75ab70cfb2ad91581d417aa802dd4aca54be044b
Tùng Phạm <phamtung240402@gmail.com>
2023-03-12 00:50:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/qlct/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.qlct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+    private void initView() {
+        mBottomNavigationView = findViewById(R.id.bottomNagivationView);
+        mBottomNavigationView.setBackground(null);
+    }
+}
