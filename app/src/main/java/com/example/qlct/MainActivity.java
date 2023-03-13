996e3f2a4e99a3c89b45605370679af4f7401dd7
Tùng Phạm <phamtung240402@gmail.com>
2023-03-14 00:44:37 +0700

    Tung commit

--- app/src/main/java/com/example/qlct/MainActivity.java
+++ app/src/main/java/com/example/qlct/MainActivity.java
@@ -11,29 +11,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView mBottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
     }
+
     private void initView() {
         mBottomNavigationView = findViewById(R.id.bottomNagivationView);
         mBottomNavigationView.setBackground(null);
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 2c32a2da8f6f27baf2538a4729eb7ed4f19cb47b
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2c32a2da8f6f27baf2538a4729eb7ed4f19cb47b
